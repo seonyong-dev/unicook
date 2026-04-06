3b186024f84db1ab2123359db661d8a5a769c3c6
bsw <bsw0329@gmail.com>
2026-04-06 14:38:40 +0900

    UserDAO 수정

--- modules/UserDAO.py
+++ modules/UserDAO.py
@@ -7,16 +7,15 @@ from modules.DBManager import DBManager
 class UserDAO :
     def Login(self, id, pw) :
         
-        vo = None
-        
-        sql  = "select * from user "
-        sql += f"where id = '{id}' and pw = md5('{pw}')"
-        dbms = DBManager()
-        dbms.DBOpen()
-        total = dbms.Select(sql)
-        if total >= 0 :
-            vo = UserVO()
-            vo.id = dbms.GetValue(0,"id")
-            vo.name   = dbms.GetValue(0,"name")
-        dbms.DBClose()
-        return vo
+        with DBManager() as db :
+            vo = None
+            
+            sql  = "select * from user "
+            sql += f"where id = '{id}' and pw = md5('{pw}')"
+            
+            total = db.Select(sql)
+            if total >= 0 :
+                vo = UserVO()
+                vo.id = db.GetValue(0,"id")
+                vo.name   = db.GetValue(0,"name")
+            return vo
